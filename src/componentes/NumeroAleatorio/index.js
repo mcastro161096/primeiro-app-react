b770296744a70133390b7bfe589961e831856dc3
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-05-14 19:37:34 -0300

    Finalizado o componente de numeros aleatórios.

--- src/componentes/NumeroAleatorio/index.js
+++ src/componentes/NumeroAleatorio/index.js
@@ -2,19 +2,29 @@ import React, { useState } from 'react';
 import './style.css';
 
 export default function NumeroAleatorio() {
-    const [numeroaleatorio, setNumero] = useState(1);
+    const [numeroaleatorio, setNumero] = useState(0);
+
+    function gerar(){
+        const novo = Math.floor(Math.random() * (100 - 1) + 1);
+        setNumero(novo);
+    }
+
     return(
-        <div className="conteudo">
+        <div className="card-numeroaleatorio">
+            <div>
+            <p>
             <h3>Número aleatório:</h3>
             <h1>{numeroaleatorio}</h1>
-            <div className="botao">
-            <label>
+            </p>
+            
+            <p>
                 Click no botão para gerar um numero aleatório
-            </label>
-            <button>
+            </p>
+            <button onClick={gerar}>
                 Gerar número
             </button>
             </div>
+            
         </div>
     );
 }
