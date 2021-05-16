1d783e1fdbaffbb7f8262a58e081fb4b06409322
Matheus Castro Oliveira <61395108+mcastro161096@users.noreply.github.com>
2021-05-16 01:05:24 -0300

    Estudando rotas.

---
+++ src/componentes/BotaoVoltar/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './style.css';
+
+const BotaoVoltar = () => (
+    <div className="area-botao">
+        <a className="botao-voltar" href='/'>Voltar</a>
+    </div>
+);
+export default BotaoVoltar;
